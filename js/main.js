85e45dde1aaf9d37c78f2a6f4bc08a3fd70724d9
Zimnos <maxence.veilleux@gmail.com>
2025-02-26 01:57:37 -0500

    Sections completed & added construction flags
    • Fixed non-autoplaying video
    • Added « Prix d'excellence » section
    • Added « in construction » messages on the website where needed
    • Completed the YouTube section
    • Completed the Game Jams section

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
 // alert("Hello World!");
+let banniereConstruction = document.getElementById("banniere-construction");
+
+banniereConstruction.addEventListener("click", e => {
+    banniereConstruction.style.display = "none";
+    document.documentElement.style.setProperty("--decalage-construction", 0);
+});
