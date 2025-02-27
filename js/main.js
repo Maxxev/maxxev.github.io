909a97b0c6ee4c25a89e59e1e28d57527927492f
Zimnos <maxence.veilleux@gmail.com>
2025-02-27 01:54:24 -0500

    Complétion du contenu du site
    - Section graphisme complétée
    - Section formation enlevée
    - Modification de la structure des marges dans les articles
    - Complétion du footer
    - Fixes et changements mineurs

--- js/main.js
+++ js/main.js
@@ -1,7 +1,40 @@
-// alert("Hello World!");
-let banniereConstruction = document.getElementById("banniere-construction");
+const banniereConstruction = document.getElementById("banniere-construction");
 
 banniereConstruction.addEventListener("click", e => {
     banniereConstruction.style.display = "none";
     document.documentElement.style.setProperty("--decalage-construction", 0);
 });
+
+
+
+// CAROUSEL
+
+const carouselBack = document.getElementById("carousel-back");
+const carouselNext = document.getElementById("carousel-next");
+const carouselImgs = document.getElementById("carousel-art").children;
+const pathToArt = "images/graphisme/";
+const imgAmount = 11;
+let currentImg = 0;
+
+switchCarouselImgs(0);
+
+carouselBack.addEventListener("click", e => {
+    switchCarouselImgs(-1);
+});
+
+carouselNext.addEventListener("click", e => {
+    switchCarouselImgs(1);
+});
+
+function switchCarouselImgs(amount) {
+    currentImg = mod(currentImg + amount, imgAmount);
+    carouselImgs[0].style.backgroundImage = `url(${pathToArt}${mod(currentImg - 1, imgAmount)}.png)`;
+    carouselImgs[1].style.backgroundImage = `url(${pathToArt}${currentImg}.png)`;
+    carouselImgs[2].style.backgroundImage = `url(${pathToArt}${mod(currentImg + 1, imgAmount)}.png)`;
+}
+
+function mod(x, y) {
+    while (x < 0)
+        x += y;
+    return x % y;
+}
